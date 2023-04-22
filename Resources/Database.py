5f9d9221d41c5b610df9b5e69a55fcbea737b576
Shazan Fazal <shazanfazal7@gmail.com>
2023-04-22 21:05:18 +0530

    [feature/updated the c db connection]

--- Resources/Database.py
+++ Resources/Database.py
@@ -66,6 +66,7 @@ def db_connection(app):
     build_uri = f"postgresql://{config.get('user')}:{config.get('password')}@{config.get('host')}/{config.get('name')}"
     app.logger.info(build_uri)
     app.config['SQLALCHEMY_DATABASE_URI'] = build_uri
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     with app.app_context():
         db.create_all()
